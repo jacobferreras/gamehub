a8df5bd65a0fadbaebc0fa86881e271e67e56a5c
BONGSTOP <152048667+jacobferreras@users.noreply.github.com>
2025-05-03 13:53:31 +0800

    Enhance HomePage.jsx with motion effects for improved user engagement and responsiveness

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,68 +1,98 @@
 import React from "react";
-// import HomeBg from "../assets/HomeBg.png";
-// // import HomeBgMobile from "../assets/HomeBgMobile.png";
+import { motion } from "motion/react";
 import ValorantIcon from "../assets/valorant_icon.png";
 import DotaIcon from "../assets/dota2_icon.png";
 import NbaIcon from "../assets/2k_logo.png";
 
-<link
-  href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap"
-  rel="stylesheet"
-></link>;
-
 const HomePage = () => {
   return (
-    <div className="bg-cover bg-no-repeat bg-center min-h-screen bg-black opacity-90 bg-[url('/assets/HomeBgMobile.png')]  sm:bg-[url('/assets/HomeBg640.png')]  lg:bg-[url('/assets/HomeBg1024.png')]  2xl:bg-[url('/assets/HomeBg.png')]">
-      <div className="absolute inset-0 items-center justify-center text-center pt-55 lg:text-start lg:justify-center lg:items-center lg:px-4 lg:pt-38 ">
-        <h1 className="font-bold text-5xl text-white font-[Bebas Neue] pb-5 sm:pt-30 sm:text-6xl md:text-7xl md:pb-2 lg:text-7xl lg:pr-80 lg:pb-2 2xl:text-8xl 2xl:pr-300 2xl:pl-8">
+    <div className="bg-cover bg-no-repeat bg-center min-h-screen bg-black opacity-90 bg-[url('/assets/HomeBgMobile.png')] sm:bg-[url('/assets/HomeBg640.png')] lg:bg-[url('/assets/HomeBg1024.png')] 3xl:bg-[url('/assets/HomeBg.png')]">
+      <div className="absolute inset-0 items-center justify-center text-center pt-55 lg:text-start lg:justify-center lg:items-center lg:px-4 lg:pt-38">
+        <motion.h1
+          className="font-bold text-5xl text-white font-[Bebas Neue] pb-5 sm:pt-30 sm:text-6xl md:text-7xl md:pb-2 lg:text-7xl lg:pr-80 lg:pb-2 xl:pl-4 xl:text-7xl xl:pr-100 3xl:text-8xl 3xl:pr-300 3xl:pl-8"
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
           Welcome to GameHub
-        </h1>
-        <h2 className=" text-lg text-white font-semibold pb-5 px-4 sm:text-xl lg:text-lg lg:pl-2 lg:pr-158 2xl:pl-10 2xl:text-2xl ">
+        </motion.h1>
+
+        <motion.h2
+          className="text-lg text-white font-semibold pb-5 px-4 sm:text-xl lg:text-lg lg:pl-2 lg:pr-158 xl:pl-6 xl:text-xl 3xl:pl-10 3xl:text-2xl"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 0.5 }}
+        >
           Tournaments, News, Builds, and Community — All in One Place
-        </h2>
-        <div className="lg:pl-2 2xl:pl-10">
-          <button className="btn bg-transparent backdrop-blur-sm border-white font-bold 2xl:btn-xl">
+        </motion.h2>
+
+        <motion.div
+          className="lg:pl-2 xl:pl-6 3xl:pl-10"
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 0.5, delay: 1 }}
+        >
+          <motion.button
+            className="btn bg-transparent backdrop-blur-sm border-white font-bold 3xl:btn-xl"
+            whileHover={{
+              scale: 1.1,
+              backgroundColor: "rgba(255, 255, 255, 0.2)",
+            }}
+            whileTap={{ scale: 0.9 }}
+          >
             ENTER HUB
-          </button>
-        </div>
+          </motion.button>
+        </motion.div>
       </div>
 
-      <div className="pt-134 flex flex-col justify-center items-center gap-2 sm:flex-row sm:pt-168 sm:gap-1 sm:justify-around xs:px-4 sm:px-2 md:px-4 lg:px-10 lg:gap-0 2xl:pt-154 2xl:flex-wrap 2xl:flex-row 2xl:pr-280 2xl:gap-y-4 ">
-        <div className="card w-96 bg-base-100 card-xs shadow-sm xs:w-full sm:w-86 md:w-auto 2xl:w-86">
+      <div className="pt-134 flex flex-col justify-center items-center gap-2 sm:flex-row sm:pt-168 sm:gap-1 sm:justify-around xs:px-4 sm:px-2 md:px-4 lg:px-12 lg:gap-0 xl:px-24 3xl:pt-154 3xl:items-start 3xl:place-items-start 3xl:flex-wrap 3xl:flex-row 3xl:pr-280 3xl:gap-y-4">
+        <motion.div
+          className="card w-96 bg-base-100 card-xs shadow-sm xs:w-full sm:w-86 md:w-auto"
+          whileHover={{ scale: 1.08 }}
+          whileTap={{ scale: 0.95 }}
+        >
           <div className="card-body flex flex-row items-center justify-start">
             <figure>
               <img src={ValorantIcon} alt="" className="rounded-xl" />
             </figure>
             <div className="flex flex-col">
               <h1 className="card-title text-sm font-bold">Valorant</h1>
-              <p>Agent Guides, News & Updates, and Patch Notes,</p>
+              <p>Agent Guides, News & Updates, and Patch Notes</p>
             </div>
           </div>
-        </div>
+        </motion.div>
 
-        <div className="card w-96 bg-base-100 card-xs shadow-sm xs:w-full sm:w-auto md:w-auto 2xl:w-86">
+        <motion.div
+          className="card w-96 bg-base-100 card-xs shadow-sm xs:w-full sm:w-86 md:w-auto"
+          whileHover={{ scale: 1.08 }}
+          whileTap={{ scale: 0.95 }}
+        >
           <div className="card-body flex flex-row items-center justify-start">
             <figure>
               <img src={DotaIcon} alt="" className="rounded-xl" />
             </figure>
             <div className="flex flex-col">
               <h1 className="card-title text-sm font-bold">Dota 2</h1>
-              <p>Hero Guide, News & Esports, and Patch Notes</p>
+              <p>Agent Guides, News & Updates, and Patch Notes</p>
             </div>
           </div>
-        </div>
+        </motion.div>
 
-        <div className="card w-96 bg-base-100 card-xs shadow-sm xs:w-full sm:w-auto md:w-auto 2xl:w-86">
+        <motion.div
+          className="card w-96 bg-base-100 card-xs shadow-sm xs:w-full sm:w-86 md:w-auto"
+          whileHover={{ scale: 1.08 }}
+          whileTap={{ scale: 1.05 }}
+        >
           <div className="card-body flex flex-row items-center justify-start">
             <figure>
               <img src={NbaIcon} alt="" className="rounded-xl" />
             </figure>
             <div className="flex flex-col">
               <h1 className="card-title text-sm font-bold">NBA2K</h1>
-              <p>Build Lab, 2K Newsroom, Gameplay Patch Notes</p>
+              <p>Agent Guides, News & Updates, and Patch Notes</p>
             </div>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
