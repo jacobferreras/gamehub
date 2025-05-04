965f50638e89e2f4290267f3359c1ce6d477c07b
BONGSTOP <152048667+jacobferreras@users.noreply.github.com>
2025-05-04 15:41:47 +0800

    Implement HomePage layout with background animation and icon displays

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import BgAnimation from "../assets/DarkBg.png";
+import JuggernutIcon from "../assets/JuggernutIcon.png";
+import JettIcon from "../assets/JettIcon.png";
+import SupplyIcon from "../assets/SupplyIcon.png";
 
 const HomePage = () => {
-  return <div className="bg-black min-h-screen"></div>;
+  return (
+    <div
+      className="hero min-h-screen relative"
+      style={{
+        backgroundImage: `url(${BgAnimation})`,
+      }}
+    >
+      {/* Juggernut Icon */}
+      <div className="absolute juggernut-icon">
+        <img
+          className="h-180 animate-pan-left"
+          src={JuggernutIcon}
+          alt="Juggernut Icon"
+        />
+      </div>
+
+      {/* Jett Icon */}
+      <div className="absolute jett-icon">
+        <img
+          className="h-130 animate-pan-right"
+          src={JettIcon}
+          alt="Jett Icon"
+        />
+      </div>
+
+      {/* Supply Icon */}
+      <div className="absolute supply-icon">
+        <img
+          className="h-100 animate-pan-up"
+          src={SupplyIcon}
+          alt="Supply Icon"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
