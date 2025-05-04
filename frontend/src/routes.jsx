5bde7e9ff2d6db6db97ddf62380e749bc6320b91
BONGSTOP <152048667+jacobferreras@users.noreply.github.com>
2025-05-04 19:34:17 +0800

    Fix import order in routes.jsx and update landing page route

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,16 +1,16 @@
 import { Navigate, Routes, Route } from "react-router-dom";
 
 import ValorantAccountScreen from "./pages/ValorantAccountScreen";
-import HomePage from "./pages/HomePage";
+import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import Dota2AccountScreen from "./pages/Dota2AccountScreen";
-import LandingPage from "./pages/LandingPage";
+import HomePage from "./pages/HomePage";
 import Layout from "./layout";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/app/landing" element={<LandingPage />} />
+      {/* <Route path="/app/home" element={<HomePage />} /> */}
       <Route path="/app" element={<Layout />}>
         <Route path="home" element={<HomePage />} />
         <Route path="valorant" element={<ValorantAccountScreen />} />
