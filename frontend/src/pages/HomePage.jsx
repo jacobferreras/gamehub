4011930329693e5c00d5a0fabc34b7e835080849
BONGSTOP <152048667+jacobferreras@users.noreply.github.com>
2025-05-06 16:52:53 +0800

    Update HomePage component: enhance layout, add latest news section, and integrate HeadNewsCard and NewsCard components

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,87 +4,134 @@ import { Link } from "react-router-dom";
 import ValorantIcon from "../assets/valorant_icon.png";
 import DotaIcon from "../assets/dota2_icon.png";
 import PubgIcon from "../assets/PubgIcon.png";
+import HeadNewsCard from "../components/common/HeadNewsCard";
+import NewsCard from "../components/common/NewsCard";
 
 const HomePage = () => {
   return (
-    <div className="bg-cover bg-no-repeat bg-center min-h-screen bg-black opacity-90 bg-[url('/assets/HomeBgMobile.png')] sm:bg-[url('/assets/HomeBg640.png')] lg:bg-[url('/assets/HomeBg1024.png')] 3xl:bg-[url('/assets/HomeBg.png')]">
-      <div className="absolute inset-0 items-center justify-center text-center pt-55 lg:text-start lg:justify-center lg:items-center lg:px-4 lg:pt-38">
-        <motion.h1
-          className="font-bold  text-5xl text-white font-[Bebas Neue] pb-5 sm:pt-30 sm:text-6xl md:text-7xl md:pb-2 lg:text-7xl lg:pr-80 lg:pb-2 xl:pl-4 xl:text-7xl xl:pr-100 3xl:text-8xl 3xl:pr-300 3xl:pl-2"
-          initial={{ opacity: 0, y: -50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1 }}
-        >
-          Welcome to GameHub
-        </motion.h1>
+    <>
+      <div className="bg-cover bg-no-repeat bg-center min-h-screen bg-[url('/assets/Bg.png')] ">
+        <div className="absolute inset-0 items-center justify-center text-center pt-55 md:pt-54 lg:text-start lg:justify-center lg:items-center lg:px-4 lg:pt-54 xl:pt-62 3xl:pt-62">
+          <motion.h1
+            className="font-bold text-center text-6xl text-zinc-100 font-[Bebas Neue] pb-2 sm:pt-30 sm:text-6xl md:text-6xl md:pb-2 lg:text-7xl  lg:pb-2  xl:text-8xl  3xl:text-9xl "
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+          >
+            Welcome to GameHub
+          </motion.h1>
 
-        <motion.h2
-          className="text-lg text-white font-semibold pb-5 px-4 sm:text-xl lg:text-lg lg:pl-2 lg:pr-158 xl:pl-6 xl:text-xl 3xl:pl-4 3xl:text-2xl"
-          initial={{ opacity: 0, y: 50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1, delay: 0.5 }}
-        >
-          Tournaments, News, Builds, and Community — All in One Place
-        </motion.h2>
+          <motion.h2
+            className="text-lg text-center text-zinc-100 font-semibold pb-5 px-4 sm:text-sm md:xl lg:text-xl  xl:text-2xl  3xl:text-3xl"
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.5 }}
+          >
+            Tournaments, News, Builds, and Community — All in One Place
+          </motion.h2>
 
-        <motion.div
-          className="lg:pl-2 xl:pl-6 3xl:pl-10"
-          initial={{ scale: 0 }}
-          animate={{ scale: 1 }}
-          transition={{ duration: 0.5, delay: 1 }}
-        ></motion.div>
+          <motion.div
+            className="lg:pl-2 xl:pl-6 3xl:pl-10"
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            transition={{ duration: 0.5, delay: 1 }}
+          ></motion.div>
+        </div>
+
+        <div className="pt-110 flex flex-col px-4 gap-2 xs:items-center sm:flex-row sm:pt-112  xs:px-4  md:pt-110 md:gap-2  lg:pt-120 lg:px-20 lg:gap-4 xl:pt-140 xl:px-24 3xl:pt-140 3xl:px-108">
+          <motion.div
+            className="card w-96 bg-base-100 card-xs shadow-sm sm:w-106"
+            whileHover={{ scale: 1.08 }}
+            whileTap={{ scale: 0.95 }}
+          >
+            <div className="card-body flex flex-row">
+              <figure>
+                <img src={ValorantIcon} alt="" className="rounded-xl" />
+              </figure>
+              <div className="flex flex-col">
+                <h1 className="card-title text-sm font-bold">Valorant</h1>
+                <p>Agent Guides, News & Updates, and Patch Notes</p>
+              </div>
+            </div>
+          </motion.div>
+
+          <motion.div
+            className="card w-96 bg-base-100 card-xs shadow-sm sm:w-106"
+            whileHover={{ scale: 1.08 }}
+            whileTap={{ scale: 0.95 }}
+          >
+            <div className="card-body flex flex-row items-center justify-start">
+              <figure>
+                <img src={DotaIcon} alt="" className="rounded-xl" />
+              </figure>
+              <div className="flex flex-col">
+                <h1 className="card-title text-sm font-bold">Dota 2</h1>
+                <p> Hero Guide, News & Esports, and Patch Notes</p>
+              </div>
+            </div>
+          </motion.div>
+
+          <motion.div
+            className="card w-96 bg-base-100 card-xs shadow-sm sm:w-106 3xl:w-110"
+            whileHover={{ scale: 1.08 }}
+            whileTap={{ scale: 1.05 }}
+          >
+            <div className="card-body flex flex-row">
+              <figure>
+                <img src={PubgIcon} alt="" className="rounded-xl" />
+              </figure>
+              <div className="flex flex-col">
+                <h1 className="card-title text-sm font-bold">PUBG</h1>
+                <p>Build Lab, 2K Newsroom, and Gameplay Patch Notes</p>
+              </div>
+            </div>
+          </motion.div>
+        </div>
       </div>
 
-      <div className="pt-136 flex flex-col justify-center items-center gap-2 sm:flex-row sm:pt-170 sm:gap-1 sm:justify-around xs:px-4 sm:px-2 md:px-4 lg:px-12 lg:gap-2 xl:px-24 3xl:pt-130 3xl:items-start 3xl:place-items-start 3xl:flex-wrap 3xl:flex-row 3xl:pl-6 3xl:pr-302  3xl:gap-y-4">
-        <motion.div
-          className="card w-96 bg-base-100 card-xs shadow-sm xs:w-full sm:w-86 md:w-auto"
-          whileHover={{ scale: 1.08 }}
-          whileTap={{ scale: 0.95 }}
-        >
-          <div className="card-body flex flex-row items-center justify-start">
-            <figure>
-              <img src={ValorantIcon} alt="" className="rounded-xl" />
-            </figure>
-            <div className="flex flex-col">
-              <h1 className="card-title text-sm font-bold">Valorant</h1>
-              <p>Agent Guides, News & Updates, and Patch Notes</p>
-            </div>
+      <div className="pt-8 bg-neutral-900">
+        <h1 className="flex justify-start pl-4 pb-2 text-2xl font-semibold">
+          Latest News
+        </h1>
+        <div className="flex justify-start pb-3">
+          <HeadNewsCard
+            Title="DOTA 2"
+            Description="awdawdaaswwa"
+            ImageUrl="https://static.gosugamers.net/51/ee/b9/8e1b988745d3f49092838d35bf306c4878ffe13a257acd104ab522ead8.webp?w=1600"
+          />
+        </div>
+        <div className="block px-2">
+          <div className="pb-2">
+            <NewsCard
+              Title="DOTA 2"
+              Description="ADAASDASD"
+              ImageUrl="https://hawk.live/storage/post-images/nightfall-left-tundr-esports-17946.jpg"
+            />
           </div>
-        </motion.div>
-
-        <motion.div
-          className="card w-96 bg-base-100 card-xs shadow-sm xs:w-full sm:w-86 md:w-auto"
-          whileHover={{ scale: 1.08 }}
-          whileTap={{ scale: 0.95 }}
-        >
-          <div className="card-body flex flex-row items-center justify-start">
-            <figure>
-              <img src={DotaIcon} alt="" className="rounded-xl" />
-            </figure>
-            <div className="flex flex-col">
-              <h1 className="card-title text-sm font-bold">Dota 2</h1>
-              <p> Hero Guide, News & Esports, and Patch Notes</p>
-            </div>
+          <div className="pb-2">
+            <NewsCard
+              Title="Valorant"
+              Description="ADAASDASD"
+              ImageUrl="https://od2-image-api.abs-cbn.com/prod/editorImage/1746436948630050525_Paper%20Rex.jpg"
+            />
           </div>
-        </motion.div>
-
-        <motion.div
-          className="card w-96 bg-base-100 card-xs shadow-sm xs:w-full sm:w-86 md:w-auto"
-          whileHover={{ scale: 1.08 }}
-          whileTap={{ scale: 1.05 }}
-        >
-          <div className="card-body flex flex-row items-center justify-start">
-            <figure>
-              <img src={PubgIcon} alt="" className="rounded-xl" />
-            </figure>
-            <div className="flex flex-col">
-              <h1 className="card-title text-sm font-bold">PUBG</h1>
-              <p>Build Lab, 2K Newsroom, and Gameplay Patch Notes</p>
-            </div>
+          <div className="pb-2">
+            <NewsCard
+              Title="PUBG"
+              Description="ADAASDASD"
+              ImageUrl="https://wstatic-prod-boc.krafton.com/common/news/20250504/80tgBPXe.jpg"
+            />
           </div>
-        </motion.div>
+          <div className="pb-2">
+            <NewsCard
+              Title="Valorant"
+              Description="ADAASDASD"
+              ImageUrl="https://esportsinsider.com/wp-content/uploads/2025/05/edg-valorant-masters-toronto-failure-large.jpg"
+            />
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
