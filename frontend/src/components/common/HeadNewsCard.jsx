cc67809e179f9f7f8814b1fb5bf1b5dcc411791c
BONGSTOP <152048667+jacobferreras@users.noreply.github.com>
2025-05-07 13:41:57 +0800

    Refactor NewsCard component: remove Width prop and simplify layout structure

--- frontend/src/components/common/HeadNewsCard.jsx
+++ frontend/src/components/common/HeadNewsCard.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 
-const NewsCard = ({ Title, Description, ImageUrl, Width }) => {
+const NewsCard = ({ Title, Description, ImageUrl }) => {
   return (
-    <div
-      className="card bg-transparent shadow-sm"
-      style={{ width: `${Width}px` }}
-    >
-      <figure>
-        <img src={ImageUrl} alt="Shoes" />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{Title}</h2>
-        <p>{Description}</p>
+    <div className="shadow-sm w-full relative">
+      <div className="absolute z-10 flex flex-col justify-center items-start p-4 text-white bottom-2 left-0 3xl:bottom-6">
+        <h2 className="font-bold text-lg">{Title}</h2>
+        <p className="text-sm font-semibold">{Description}</p>
+      </div>
+      <div className="relative">
+        <img
+          className="w-full h-full object-cover"
+          src={ImageUrl}
+          alt="Shoes"
+        />
+        <div className="absolute inset-0 bg-black opacity-65 hover:opacity-70"></div>
       </div>
     </div>
   );
--- frontend/src/components/common/NewsCard.jsx
+++ frontend/src/components/common/NewsCard.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 
-const NewsCard = ({ Title, Description, ImageUrl, Width }) => {
+const NewsCard = ({ Title, Description, ImageUrl }) => {
   return (
-    <div
-      className="card card-side bg-red-500 shadow-sm"
-      style={{ width: `${Width}px` }}
-    >
-      <figure>
+    <div className="card card-side bg-red-500 shadow-sm 3xl:w-160 relative">
+      <figure className="relative w-[150px] h-[150px]">
         <img
           src={ImageUrl}
           alt="Movie"
-          style={{ width: "150px", height: "150px" }}
+          style={{ width: "150px", height: "150px", objectFit: "cover" }}
         />
+        <div className="absolute inset-0 bg-black opacity-40 hover:opacity-50"></div>
       </figure>
+
       <div className="card-body">
         <h2 className="card-title">{Title}</h2>
         <p>{Description}</p>
