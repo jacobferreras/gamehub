86db5a66ae0ab9510076630b3caae3c75e2ba58b
BONGSTOP <152048667+jacobferreras@users.noreply.github.com>
2025-05-08 14:10:24 +0800

    Remove homeController, homeRoutes, and homeService files to streamline backend code

--- backend/src/routes/homeRoutes/homeRoute.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import homeController from "../../controllers/homeController/homeController.js";
-
-const router = Router();
-
-router.get("/", homeController.getAll);
-
-export default router;
