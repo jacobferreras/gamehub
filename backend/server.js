3280dc1950690110f4a1456195c0163f20a1148f
BONGSTOP <152048667+jacobferreras@users.noreply.github.com>
2025-05-09 20:58:57 +0800

    Add article route to server for handling article-related requests

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import pkg from "body-parser";
 import { config } from "dotenv";
 import valorantAccountRoute from "./src/routes/valorantRoutes/valorantAccountRoute.js";
 import proPlayerRoute from "./src/routes/proPlayerRoutes/proPlayerRoute.js";
+import articleRoute from "./src/routes/articleRoutes/articleRoute.js";
 
 const { json } = pkg;
 
@@ -40,6 +41,7 @@ db.connect((err) => {
 //routes
 app.use("/valorantAccount", valorantAccountRoute);
 app.use("/proplayers", proPlayerRoute);
+app.use("/articles", articleRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
