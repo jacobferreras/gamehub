2958cf5defbba894db6831501a2e74db752e0d46
BONGSTOP <152048667+jacobferreras@users.noreply.github.com>
2025-05-09 17:22:11 +0800

    Refactor proPlayerService to include random selection and enhance filtering by game

--- backend/src/services/proPlayerService/proPlayerService.js
+++ backend/src/services/proPlayerService/proPlayerService.js
@@ -1,14 +1,24 @@
 import { db } from "../../../server.js";
 
-const getAll = async ({ limit = 10, game = "" }) => {
-  const query = "SELECT * FROM proplayers LIMIT ?";
-  const values = [parseInt(limit)];
-  const result = await db.promise().query(query, values);
-  if (result[0].length === 0) {
-    throw new Error("No valorant pro players found");
+const getAll = async ({ limit = 10, game = "", random = false }) => {
+  let condition = ``;
+  let sql = `SELECT * FROM proplayers`;
+  if (game) {
+    condition = ` WHERE game LIKE '%${game}%'`;
   }
+  if (condition) {
+    sql += condition;
+  }
+
+  if (random) {
+    sql += ` ORDER BY RAND()`;
+  }
+
+  sql += ` LIMIT ${limit}`;
+  const result = await db.promise().query(sql);
+
   return {
-    message: "Valorant pro players found",
+    message: "Pro players found",
     data: result[0],
   };
 };
