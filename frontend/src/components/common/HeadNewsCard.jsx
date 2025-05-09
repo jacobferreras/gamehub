5b0bdb9814ef31d8a6e74c0d87cf7c7126478bf6
BONGSTOP <152048667+jacobferreras@users.noreply.github.com>
2025-05-09 20:59:33 +0800

    Refactor HeadNewsCard component to fetch and display news data dynamically

--- frontend/src/components/common/HeadNewsCard.jsx
+++ frontend/src/components/common/HeadNewsCard.jsx
@@ -1,16 +1,42 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
-const NewsCard = ({ Title, Description, ImageUrl }) => {
+const HeadNewsCard = () => {
+  const [news, setNews] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      "https://thingproxy.freeboard.io/fetch/https://vlrggapi.vercel.app/news"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (
+          data &&
+          data.data &&
+          data.data.segments &&
+          data.data.segments.length > 0
+        ) {
+          setNews(data.data.segments[0]);
+        }
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  if (!news) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="shadow-sm w-full relative">
       <div className="absolute z-10 flex flex-col justify-center items-start p-4 text-white bottom-2 left-0 3xl:bottom-6">
-        <h2 className="font-bold text-lg">{Title}</h2>
-        <p className="text-sm font-semibold">{Description}</p>
+        <h2 className="font-bold text-lg">{news.title}</h2>
+        <p className="text-sm font-semibold">{news.date}</p>
+        <p className="text-sm font-semibold">by: {news.author}</p>
+        <p className="text-sm font-semibold">{news.description}</p>
       </div>
       <div className="relative">
         <img
           className="w-full h-auto rounded-md  sm:rounded-md md:rounded-lg lg:rounded-xl"
-          src={ImageUrl}
+          src="https://owcdn.net/img/681d0d93169d3.jpg"
           alt="Shoes"
         />
         <div className="absolute inset-0 bg-black opacity-65 hover:opacity-70 rounded-md sm:rounded-md md:rounded-xl lg:rounded-xl"></div>
@@ -19,4 +45,4 @@ const NewsCard = ({ Title, Description, ImageUrl }) => {
   );
 };
 
-export default NewsCard;
+export default HeadNewsCard;
