5102757c720769424d3998f1cc75279c9142d5a8
BONGSTOP <152048667+jacobferreras@users.noreply.github.com>
2025-05-11 10:52:16 +0800

    Add GuideSection and Game Highlights to HomePage component

--- frontend/src/pages/HomeScreen.jsx
+++ frontend/src/pages/HomeScreen.jsx
@@ -6,6 +6,7 @@ import PubgIcon from "../assets/PubgIcon.png";
 import Carousel from "../components/ui/Carousel";
 import LargeScreenNews from "../components/ui/LargeScreenNews";
 import SmallScreenNews from "../components/ui/SmallScreenNews";
+import GuideSection from "../components/ui/GuideSection";
 
 const HomePage = () => {
   return (
@@ -97,6 +98,11 @@ const HomePage = () => {
         <LargeScreenNews />
         <div className="my-6 border-t border-gray-600 opacity-50"></div>
         <Carousel />
+        <div className="my-6 border-t border-gray-600 opacity-50"></div>
+        <h1 className="flex justify-start pl-2 pb-2 text-2xl font-semibold md:pl-8 3xl:text-4xl">
+          Game Highlights
+        </h1>
+        <GuideSection />
       </div>
     </>
   );
