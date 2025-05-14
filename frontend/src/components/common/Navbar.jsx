215ddc64ac79bdb7e8c3abf5b289f587bd7a5c24
BONGSTOP <152048667+jacobferreras@users.noreply.github.com>
2025-05-14 17:55:14 +0800

    Refactor Navbar component to use a drawer layout and update menu items

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -3,66 +3,75 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-transparent backdrop-blur-sm fixed top-0 z-10">
-      <div className="navbar-start">
-        <div className="dropdown block lg:hidden xl:hidden 2xl:hidden 3xl:hidden">
-          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+    <div className="drawer">
+      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col">
+        {/* Navbar */}
+        <div className="navbar bg-transparent backdrop-blur-md fixed top-0 z-10 w-full lg:px-20">
+          <div className="flex-none lg:hidden">
+            <label
+              htmlFor="my-drawer-3"
+              aria-label="open sidebar"
+              className="btn btn-square btn-ghost"
             >
-              {" "}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h7"
-              />{" "}
-            </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block h-6 w-6 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+            </label>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <Link to="/app/home">HOME</Link>
-            </li>
-
-            <li>
-              <Link to="/app/valorant">VALORANT</Link>
-            </li>
-
-            <li>
-              <Link to="/app/dota2">DOTA 2</Link>
-            </li>
-            <li>
-              <Link to="/app/pubg">PUBG</Link>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <a className="btn btn-ghost text-xl hidden lg:block xl:block 2xl:block 3xl:block">
+          <div className="mx-2 flex-1 px-2 text-3xl font-semibold text-center lg:text-start">
             GameHub
-          </a>
+          </div>
+          <div className="hidden flex-none lg:block">
+            <ul className="menu menu-horizontal font-semibold px-1">
+              <li>
+                <Link to="/app/home">HOME</Link>
+              </li>
+              <li>
+                <Link to="/app/valorant">MATCHES</Link>
+              </li>
+              <li>
+                <Link to="/app/dota2">NEWS</Link>
+              </li>
+              <li>
+                <Link to="/app/pubg">GUIDES</Link>
+              </li>
+              <li>
+                <Link to="/app/pubg">RANKINGS</Link>
+              </li>
+              <li>
+                <Link to="/app/pubg">HIGHLIGHTS</Link>
+              </li>
+              <li>
+                <Link to="/app/pubg">PLAYERS</Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-
-      <div className="navbar-center">
-        <a className="btn btn-ghost text-xl block lg:hidden xl:hidden 2xl:hidden 3xl:hidden">
-          GameHub
-        </a>
-        <ul className="menu menu-horizontal px-1 hidden lg:flex xl:flex 2xl:flex 3xl:flex">
+      <div className="drawer-side">
+        <label
+          htmlFor="my-drawer-3"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+        <ul className="menu bg-neutral-950 min-h-full w-80 p-4 pt-20">
           <li>
             <Link to="/app/home">HOME</Link>
           </li>
-
           <li>
             <Link to="/app/valorant">VALORANT</Link>
           </li>
-
           <li>
             <Link to="/app/dota2">DOTA 2</Link>
           </li>
@@ -70,40 +79,6 @@ const Navbar = () => {
             <Link to="/app/pubg">PUBG</Link>
           </li>
         </ul>
-      </div>
-
-      <div className="navbar-end">
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-              />
-            </div>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
-        </div>
       </div>
     </div>
   );
