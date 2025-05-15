a7952c43166896d56629ec62c0d12e286355402f
BONGSTOP <152048667+jacobferreras@users.noreply.github.com>
2025-05-15 22:00:35 +0800

    Enhance article retrieval by adding pagination support in getAll function

--- backend/src/controllers/articleController/articleController.js
+++ backend/src/controllers/articleController/articleController.js
@@ -2,10 +2,12 @@ import articleService from "../../services/articleService/articleService.js";
 
 const getAll = async (req, res, next) => {
   try {
+    const page = req.query.page || 1;
     const limit = req.query.limit || 10;
     const type = req.query.type || "";
 
     const result = await articleService.getAll({
+      page: page,
       limit: limit,
       type: type,
     });
--- backend/src/services/articleService/articleService.js
+++ backend/src/services/articleService/articleService.js
@@ -1,19 +1,35 @@
 import { db } from "../../../server.js";
 
-const getAll = async ({ limit = 10, type = "" }) => {
-  let sql = `SELECT * FROM valorant_article`;
+const getAll = async ({ page = 1, limit = 10, type = "" }) => {
+  const offset = (page - 1) * limit;
+
+  let condition = ``;
   let params = [];
+
   if (type) {
-    sql += ` WHERE type LIKE ?`;
+    condition += ` WHERE type LIKE ?`;
     params.push(`%${type}%`);
   }
-  sql += ` ORDER BY created_at DESC LIMIT ?`;
+
+  let sql = `SELECT * FROM valorant_article${condition} ORDER BY created_at DESC`;
+
+  sql += ` LIMIT ? OFFSET ?`;
   params.push(Number(limit));
+  params.push(Number(offset));
+
   const result = await db.promise().query(sql, params);
+
+  const totalCountQuery =
+    `SELECT COUNT(*) as total FROM valorant_article` + condition;
+  const totalCountResult = await db.promise().query(totalCountQuery, params);
+  const totalCount = totalCountResult[0][0].total;
+  const totalPages = Math.ceil(totalCount / limit);
 
   return {
     message: "Articles found",
     data: result[0],
+    totalPages,
+    currentPage: page,
   };
 };
 
