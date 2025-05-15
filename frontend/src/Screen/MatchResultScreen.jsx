c04a08fa64263675e6d5d15c5328ea5f90597068
BONGSTOP <152048667+jacobferreras@users.noreply.github.com>
2025-05-15 09:50:27 +0800

    Implement match results fetching and display in MatchResultScreen

--- frontend/src/Screen/MatchResultScreen.jsx
+++ frontend/src/Screen/MatchResultScreen.jsx
@@ -1,7 +1,92 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Pagination from "../components/common/Pagination";
+import DropdownInputField from "../components/ui/DropdownInputField";
 
 const MatchResultScreen = () => {
-  return <div className="bg-neutral-950 min-h-screen">MatchResultScreen</div>;
+  const [schedules, setSchedules] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [region, setRegion] = useState("");
+  const limit = 8;
+
+  useEffect(() => {
+    const fetchSchedules = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:5000/matchResults?page=${currentPage}&limit=${limit}&region=${region}`
+        );
+        console.log("API Response:", response.data);
+        setSchedules(
+          Array.isArray(response.data.data) ? response.data.data : []
+        );
+        setTotalPages(response.data.totalPages || 1);
+      } catch (error) {
+        console.error("Error fetching schedules:", error);
+        setSchedules([]);
+      }
+    };
+
+    fetchSchedules();
+  }, [currentPage, limit, region]);
+  return (
+    <div className="bg-neutral-950 min-h-screen">
+      <div className="flex justify-end gap-3 pt-20 pb-10 pr-30 ">
+        <DropdownInputField
+          value={region}
+          onChange={(e) => setRegion(e.target.value)}
+        />
+      </div>
+      <div className="px-8 pb-2 gap-4 md:px-4 md:gap-y-0 lg:gap-x-2 lg:gap-y-0 lg:px-2 grid grid-cols-1 sm:grid-cols-2 3xl:grid-cols-4 3xl:px-34">
+        {schedules.map((schedule, index) => (
+          <div
+            key={schedule.id || index}
+            className="card bg-zinc-800 w-auto lg:w-auto 3xl:w-auto shadow-xl text-center text-white items-center mb-5"
+          >
+            <h2 className="card-title mt-2 text-lg px-2">
+              {schedule.event_name}
+            </h2>
+            <div className="card-body items-center text-center min-h-64 flex flex-col justify-center ">
+              <div className="flex w-full">
+                {/* Team 1 */}
+                <div className="flex flex-col items-center w-1/3">
+                  <img
+                    src={schedule.logo1}
+                    alt={schedule.team1}
+                    className="team-logo w-20 h-20 object-contain"
+                  />
+                  <p className="team-name text-sm mt-2">{schedule.team1}</p>
+                </div>
+                {/* Score Section */}
+                <div className="flex flex-col items-center justify-center w-1/3 mx-10">
+                  <h1 className="font-bold texl-3xl">Final</h1>
+                  <h2>
+                    {schedule.score1} : {schedule.score2}
+                  </h2>
+                </div>
+                {/* Team 2 */}
+                <div className="flex flex-col items-center w-1/3">
+                  <img
+                    src={schedule.logo2}
+                    alt={schedule.team2}
+                    className="team-logo w-20 h-20 object-contain"
+                  />
+                  <p className="team-name text-sm mt-2">{schedule.team2}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="flex justify-center">
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={setCurrentPage}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default MatchResultScreen;
