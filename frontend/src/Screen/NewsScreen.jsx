f2c1929777c794ad37cd8bfa565df8071e9313c3
BONGSTOP <152048667+jacobferreras@users.noreply.github.com>
2025-05-16 17:25:17 +0800

    Add background image and buttons to NewsScreen for improved UI and navigation

--- frontend/src/Screen/NewsScreen.jsx
+++ frontend/src/Screen/NewsScreen.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Pagination from "../components/common/Pagination";
+import DropdownInputField from "../components/ui/DropdownInputField";
+import NewsBg from "../assets/NewsBg.png";
 
 const NewsScreen = () => {
   const [news, setNews] = useState([]);
@@ -26,42 +28,60 @@ const NewsScreen = () => {
 
   return (
     <div className="bg-neutral-950 min-h-screen ">
-      <div className="px-8 pt-36 pb-2 gap-4 md:px-4 md:gap-y-4 lg:gap-x-4 lg:gap-y-4 lg:px-2 grid grid-cols-1 sm:grid-cols-2 3xl:grid-cols-4 3xl:px-10">
-        {news.map((article, index) => (
-          <div
-            key={article.id || index}
-            className="card bg-base-100 image-fit w-auto lg:w-full shadow-sm relative group overflow-hidden"
-          >
-            <figure className="relative">
-              <img
-                src={article.image}
-                alt="Shoes"
-                className="w-full h-64 object-cover rounded-md"
-              />
-              <div className="absolute inset-0 bg-black opacity-60 group-hover:opacity-70 transition-opacity duration-200 rounded-md"></div>
-            </figure>
+      <div className="flex justify-start gap-2 pl-10 pt-24">
+        <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">
+          Responsive
+        </button>
+        <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">
+          Responsive
+        </button>
+      </div>
+      <div className="border-t border-gray-600 opacity-50"></div>
+      <div
+        style={{
+          backgroundImage: `url(${NewsBg})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div className="px-8 pt-4 pb-2 gap-4 md:px-4 md:gap-y-4 lg:gap-x-4 lg:gap-y-4 lg:px-2 grid grid-cols-1 sm:grid-cols-2 3xl:grid-cols-4 3xl:px-10">
+          {news.map((article, index) => (
+            <div
+              key={article.id || index}
+              className="card bg-base-100 image-fit w-auto lg:w-full shadow-sm relative group overflow-hidden"
+            >
+              <figure className="relative">
+                <img
+                  src={article.image}
+                  alt="Shoes"
+                  className="w-full h-64 object-cover rounded-md"
+                />
+                <div className="absolute inset-0 bg-black opacity-60 group-hover:opacity-70 transition-opacity duration-200 rounded-md"></div>
+              </figure>
 
-            <div className="absolute bottom-0 w-full z-10 px-2 pointer-events-none">
-              <div className="transition-all duration-500 ease-in-out delay-100">
-                <h1 className="text-xs text-white">{article.date}</h1>
-                <h2 className="card-title text-xl text-white">
-                  {article.title}
-                </h2>
-                <div className="max-h-0 opacity-0 overflow-hidden transition-all duration-500 ease-in-out delay-100 group-hover:max-h-32 group-hover:opacity-100">
-                  <p className="text-sm text-white">by: {article.author}</p>
-                  <p className="text-sm text-white">{article.description}</p>
+              <div className="absolute bottom-0 w-full z-10 px-2 pointer-events-none">
+                <div className="transition-all duration-500 ease-in-out delay-100">
+                  <h1 className="text-xs text-white">{article.date}</h1>
+                  <h2 className="card-title text-xl text-white">
+                    {article.title}
+                  </h2>
+                  <div className="max-h-0 opacity-0 overflow-hidden transition-all duration-500 ease-in-out delay-100 group-hover:max-h-32 group-hover:opacity-100">
+                    <p className="text-sm text-white">by: {article.author}</p>
+                    <p className="text-sm text-white">{article.description}</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
-      <div className="flex justify-center py-4">
-        <Pagination
-          currentPage={currentPage}
-          totalPages={totalPages}
-          onPageChange={setCurrentPage}
-        />
+          ))}
+        </div>
+        <div className="flex justify-center py-4">
+          <Pagination
+            currentPage={currentPage}
+            totalPages={totalPages}
+            onPageChange={setCurrentPage}
+          />
+        </div>
       </div>
     </div>
   );
