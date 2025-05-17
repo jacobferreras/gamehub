503375e69574f0ba3baee562dd86d63a60ca14c6
BONGSTOP <152048667+jacobferreras@users.noreply.github.com>
2025-05-17 22:02:58 +0800

    Refactor NewsScreen and UpdateScreen layout for improved structure and responsiveness

--- frontend/src/Screen/UpdateScreen.jsx
+++ frontend/src/Screen/UpdateScreen.jsx
@@ -28,36 +28,39 @@ const UpdateScreen = () => {
   return (
     <div>
       <div>
-        <div className="px-8 pt-2 pb-2 gap-4 md:px-4 md:gap-y-4 lg:gap-x-4 lg:gap-y-4 lg:px-2 grid grid-cols-1 sm:grid-cols-2 3xl:grid-cols-4 3xl:px-10">
-          {updates.map((update, index) => (
-            <div
-              key={update.id || index}
-              className="card bg-base-100 image-fit w-auto lg:w-full shadow-sm relative group overflow-hidden"
-            >
-              <figure className="relative">
-                <img
-                  src={update.image}
-                  alt="Shoes"
-                  className="w-full h-64 object-cover rounded-md"
-                />
-                <div className="absolute inset-0 bg-black opacity-60 group-hover:opacity-70 transition-opacity duration-200 rounded-md"></div>
-              </figure>
+        <div className="flex-grow">
+          <div className="px-8 pt-2 pb-2 gap-4 md:px-4 md:gap-y-4 lg:gap-x-4 lg:gap-y-4 lg:px-2 grid grid-cols-1 sm:grid-cols-2 3xl:grid-cols-4 3xl:px-10">
+            {updates.map((update, index) => (
+              <div
+                key={update.id || index}
+                className="card bg-base-100 image-fit w-auto lg:w-full shadow-sm relative group overflow-hidden"
+              >
+                <figure className="relative">
+                  <img
+                    src={update.image}
+                    alt="Shoes"
+                    className="w-full h-64 object-cover rounded-md"
+                  />
+                  <div className="absolute inset-0 bg-black opacity-60 group-hover:opacity-70 transition-opacity duration-200 rounded-md"></div>
+                </figure>
 
-              <div className="absolute bottom-0 w-full z-10 px-2 pointer-events-none">
-                <div className="transition-all duration-500 ease-in-out delay-100">
-                  <h1 className="text-xs text-white">{update.date}</h1>
-                  <h2 className="card-title text-xl text-white">
-                    {update.title}
-                  </h2>
-                  <div className="max-h-0 opacity-0 overflow-hidden transition-all duration-500 ease-in-out delay-100 group-hover:max-h-32 group-hover:opacity-100">
-                    <p className="text-sm text-white"></p>
-                    <p className="text-sm text-white">{update.description}</p>
+                <div className="absolute bottom-0 w-full z-10 px-2 pointer-events-none">
+                  <div className="transition-all duration-500 ease-in-out delay-100">
+                    <h1 className="text-xs text-white">{update.date}</h1>
+                    <h2 className="card-title text-xl text-white">
+                      {update.title}
+                    </h2>
+                    <div className="max-h-0 opacity-0 overflow-hidden transition-all duration-500 ease-in-out delay-100 group-hover:max-h-32 group-hover:opacity-100">
+                      <p className="text-sm text-white"></p>
+                      <p className="text-sm text-white">{update.description}</p>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
+
         <div className="flex justify-center py-4">
           <Pagination
             currentPage={currentPage}
