8401fb753a3df727f98865e48ca37045c2eb9eb0
BONGSTOP <152048667+jacobferreras@users.noreply.github.com>
2025-05-18 02:30:24 +0800

    Enhance AgentFilter component with transition effects for agent selection

--- frontend/src/components/ui/AgentFilter.jsx
+++ frontend/src/components/ui/AgentFilter.jsx
@@ -8,8 +8,8 @@ const AgentFilter = ({ value, onChange }) => {
     <div>
       <div className="flex flex-row gap-x-2 justify-end items-end pr-12 pb-4">
         <button
-          className={`h-[30px] w-[30px] rounded-full border-2 border-gray-400 flex items-center justify-center text-xs font-bold cursor-pointer ${
-            value === "" ? "" : ""
+          className={`h-[30px] w-[30px] rounded-full border-2 border-gray-400 flex items-center justify-center text-xs font-bold cursor-pointer transition-transform duration-200 ease-in-out ${
+            value === "" ? "scale-150" : ""
           }`}
           onClick={() => onChange({ target: { value: "" } })}
         >
@@ -18,25 +18,25 @@ const AgentFilter = ({ value, onChange }) => {
         <img
           src={Duelist}
           alt="Duelist"
-          className={`h-[30px] w-[30px] cursor-pointer  ${value === "Duelist" ? "" : ""}`}
+          className={`h-[30px] w-[30px] cursor-pointer transition-transform duration-200 ease-in-out  ${value === "Duelist" ? "scale-140" : ""}`}
           onClick={() => onChange({ target: { value: "Duelist" } })}
         />
         <img
           src={Sentinel}
           alt="Sentinel"
-          className={`h-[30px] w-[30px] cursor-pointer ${value === "Sentinel" ? "" : ""}`}
+          className={`h-[30px] w-[30px] cursor-pointer transition-transform duration-200 ease-in-out ${value === "Sentinel" ? "scale-140" : ""}`}
           onClick={() => onChange({ target: { value: "Sentinel" } })}
         />
         <img
           src={Controller}
           alt="Controller"
-          className={`h-[30px] w-[30px] cursor-pointer ${value === "Controller" ? "" : ""}`}
+          className={`h-[30px] w-[30px] cursor-pointer transition-transform duration-200 ease-in-out ${value === "Controller" ? "scale-140 " : ""}`}
           onClick={() => onChange({ target: { value: "Controller" } })}
         />
         <img
           src={Initiator}
           alt="Initiator"
-          className={`h-[30px] w-[30px] cursor-pointer ${value === "Initiator" ? "" : ""}`}
+          className={`h-[30px] w-[30px] cursor-pointer transition-transform duration-200 ease-in-out ${value === "Initiator" ? "scale-140" : ""}`}
           onClick={() => onChange({ target: { value: "Initiator" } })}
         />
       </div>
