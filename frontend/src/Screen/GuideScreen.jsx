2e5715d9fffd04c71037339b60c76e70cc151c28
BONGSTOP <152048667+jacobferreras@users.noreply.github.com>
2025-05-17 22:02:54 +0800

    Refactor GuideScreen to enhance layout and styling, adding a background color and centering the title

--- frontend/src/Screen/GuideScreen.jsx
+++ frontend/src/Screen/GuideScreen.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const GuideScreen = () => {
-  return <div>PubgScreen</div>;
+  return (
+    <div className="min-h-screen bg-neutral-950">
+      <div className="flex justify-center items-center h-screen">
+        <h1>CHOOSE YOUR AGENT</h1>
+      </div>
+    </div>
+  );
 };
 
 export default GuideScreen;
