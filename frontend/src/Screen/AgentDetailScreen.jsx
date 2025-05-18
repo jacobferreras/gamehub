c0e2fb8412d43a759b2e438caa166e56dfe49b9d
BONGSTOP <152048667+jacobferreras@users.noreply.github.com>
2025-05-18 17:31:10 +0800

    Add AgentDetailScreen and enhance GuideScreen with agent links

---
+++ frontend/src/Screen/AgentDetailScreen.jsx
@@ -0,0 +1,179 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import BackArrow from "../assets/back_arrow.png";
+import { Link } from "react-router-dom"; // Adjust the path as necessary
+
+const AgentDetailScreen = () => {
+  const { id } = useParams();
+  const [agent, setAgent] = useState(null);
+  const [selectedSkill, setSelectedSkill] = useState(1);
+
+  useEffect(() => {
+    const fetchAgentDetails = async () => {
+      try {
+        const response = await axios.get(`http://localhost:5000/agents/${id}`);
+        setAgent(response.data.data);
+      } catch (error) {
+        console.error("Error fetching agent details:", error);
+      }
+    };
+
+    fetchAgentDetails();
+  }, [id]);
+
+  if (!agent) {
+    return (
+      <div className="min-h-screen bg-neutral-950 pt-20">
+        <div>Loading...</div>
+      </div>
+    );
+  }
+
+  const skillData = [
+    {
+      logo: agent.skill1_logo,
+      name: agent.skill1,
+      description: agent.skill1_description,
+      video: agent.skill1_video,
+    },
+    {
+      logo: agent.skill2_logo,
+      name: agent.skill2,
+      description: agent.skill2_description,
+      video: agent.skill2_video,
+    },
+    {
+      logo: agent.skill3_logo,
+      name: agent.skill3,
+      description: agent.skill3_description,
+      video: agent.skill3_video,
+    },
+    {
+      logo: agent.skill4_logo,
+      name: agent.skill4,
+      description: agent.skill4_description,
+      video: agent.skill4_video,
+    },
+  ];
+
+  return (
+    <div
+      className="min-h-screen pt-18 pb-24"
+      style={{
+        backgroundImage: `url("http://localhost:5000/uploads/bg.png")`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="flex flex-row justify-start pl-4">
+        <Link to="/app/guide">
+          <img src={BackArrow} alt="Back" className="w-8 h-8 cursor-pointer" />
+        </Link>
+      </div>
+
+      {/* small screen */}
+      <div className="block lg:hidden">
+        <img
+          src={`http://localhost:5000/${agent.large_image}`}
+          alt=""
+          className="h-auto"
+        />
+        <h1 className="text-4xl text-center">{agent.name}</h1>
+        <div className="flex flex-row gap-x-2 justify-center mt-2">
+          <img src={agent.role_image} alt="" className="h-[20px] w-[20px]" />
+          <h2>{agent.role}</h2>
+        </div>
+        <p className="flex justify-center text-center">{agent.description}</p>
+        <div className="px-2 mt-4">
+          <div className="card bg-base-100 video-fit w-auto shadow-sm">
+            <video
+              src={skillData[selectedSkill - 1].video}
+              autoPlay
+              controls
+              className="rounded-lg"
+              key={skillData[selectedSkill - 1].video}
+            ></video>
+          </div>
+        </div>
+        <h1 className="text-center text-lg mt-4">
+          {skillData[selectedSkill - 1].name}
+        </h1>
+        <p className="flex justify-center text-center mt-2">
+          {skillData[selectedSkill - 1].description}
+        </p>
+
+        <div className="flex flex-row gap-x-4 justify-center mt-4">
+          {skillData.map((skill, index) => (
+            <img
+              key={index}
+              src={skill.logo}
+              alt=""
+              className={`h-auto w-[50px] cursor-pointer opacity-40  ${selectedSkill === index + 1 ? "opacity-100" : ""}`}
+              onClick={() => setSelectedSkill(index + 1)}
+            />
+          ))}
+        </div>
+      </div>
+
+      {/* larger screen */}
+      <div className="hidden lg:block">
+        <div className="flex flex-row items-stretch justify-center w-full gap-x-0">
+          <div className="flex flex-col w-[55%] 3xl:w-[65%]">
+            <div className="flex flex-row gap-x-2 justify-start pl-6">
+              <img
+                src={agent.role_image}
+                alt=""
+                className="h-[15px] w-[15px] mt-2"
+              />
+              <h2 className="text-xl">{agent.role}</h2>
+            </div>
+            <h1 className="text-8xl justify-start pl-4">{agent.name}</h1>
+            <p className="flex justify-start pl-6 text-lg mt-2">
+              {agent.description}
+            </p>
+            <div className="px-2 mt-4 pl-4">
+              <div className="card bg-base-100 video-fit w-full aspect-video shadow-sm flex items-center justify-center min-h-[300px]">
+                <video
+                  src={skillData[selectedSkill - 1].video}
+                  autoPlay
+                  className="rounded-lg w-full h-full object-cover"
+                  key={skillData[selectedSkill - 1].video}
+                  controls
+                  loop
+                ></video>
+              </div>
+              <div className="text-center w-full">
+                <h1 className="text-lg mt-4 ">
+                  {skillData[selectedSkill - 1].name}
+                </h1>
+              </div>
+              <p className="text-center w-full mt-2">
+                {skillData[selectedSkill - 1].description}
+              </p>
+              <div className="flex flex-row gap-x-4 justify-center mt-4 w-full">
+                {skillData.map((skill, index) => (
+                  <img
+                    key={index}
+                    src={skill.logo}
+                    alt=""
+                    className={`h-auto w-[50px] cursor-pointer opacity-40  ${selectedSkill === index + 1 ? "opacity-100" : ""}`}
+                    onClick={() => setSelectedSkill(index + 1)}
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+          <img
+            src={`http://localhost:5000/${agent.large_image}`}
+            alt=""
+            className="w-[45%]  h-auto rounded-lg shadow-lg self-stretch"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AgentDetailScreen;
--- frontend/src/Screen/GuideScreen.jsx
+++ frontend/src/Screen/GuideScreen.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import AgentFilter from "../components/ui/AgentFilter";
 import CustomInputField from "../components/ui/CustomInputField";
@@ -26,7 +27,7 @@ const GuideScreen = () => {
 
   return (
     <div className="min-h-screen bg-neutral-950 pt-20">
-      <div className="flex flex-row justify-end pr-10">
+      <div className="flex flex-wrap justify-center lg:justify-end lg:flex-row lg:pr-10 ">
         <AgentFilter value={role} onChange={(e) => setRole(e.target.value)} />
         <CustomInputField
           value={search}
@@ -36,7 +37,8 @@ const GuideScreen = () => {
 
       <div className="px-8 pt-2 pb-2 gap-4 md:px-4 md:gap-y-4 lg:gap-x-4 lg:gap-y-4 lg:px-2 grid grid-cols-1 sm:grid-cols-2 3xl:grid-cols-4 3xl:px-10">
         {agents.map((agent, index) => (
-          <div
+          <Link
+            to={`/app/guide/${agent.id}`}
             key={agent.id || index}
             className="card bg-base-100 image-fit w-auto lg:w-full shadow-sm relative group overflow-hidden"
           >
@@ -49,7 +51,7 @@ const GuideScreen = () => {
               <div className="absolute inset-0 bg-black opacity-20 group-hover:opacity-70 transition-opacity duration-200 rounded-md"></div>
             </figure>
 
-            <div className="absolute bottom-0 w-full z-10 px-2 pointer-events-none">
+            <div className="absolute bottom-0 w-full z-10 px-2">
               <div className="transition-all duration-500 ease-in-out delay-100">
                 <div className="max-h-0  overflow-hidden transition-all duration-500 ease-in-out delay-100 group-hover:max-h-32 group-hover:pb-4 group-hover:opacity-100">
                   <div className="flex flex-row gap-x-2">
@@ -65,7 +67,7 @@ const GuideScreen = () => {
                 </div>
               </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
