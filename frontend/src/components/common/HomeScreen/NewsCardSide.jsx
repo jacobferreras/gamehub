7b5e695a88dca24469110d2478a958a961ac7310
BONGSTOP <152048667+jacobferreras@users.noreply.github.com>
2025-05-22 04:01:49 +0800

    Update NewsCard and NewsCardSide components to use a consistent background color

--- frontend/src/components/common/HomeScreen/NewsCardSide.jsx
+++ frontend/src/components/common/HomeScreen/NewsCardSide.jsx
@@ -1,6 +1,6 @@
 const NewsCard = ({ Title, Description, Date, Author, ImageUrl }) => {
   return (
-    <div className="card bg-zinc-800 w-auto h-full shadow-sm relative">
+    <div className="card bg-neutral-900 w-auto h-full shadow-sm relative">
       <figure className="relative aspect-[16/9] w-full">
         <img src={ImageUrl} alt="News" className="w-full h-full object-cover" />
         <div className="absolute inset-0 bg-black opacity-40 hover:opacity-50"></div>
