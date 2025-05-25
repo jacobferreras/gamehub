b710bc179faf16fe69ea9c819f104da7c9321570
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-05-26 02:22:44 +0800

    Refactor ValorantGameSchedule to use fetchHomeGameSchedule for data retrieval

--- frontend/src/components/ui/HomeScreen/ValorantGameSchedule.jsx
+++ frontend/src/components/ui/HomeScreen/ValorantGameSchedule.jsx
@@ -1,40 +1,35 @@
-import React from "react";
 import { useState, useEffect } from "react";
-import axios from "axios";
+import { fetchHomeGameSchedule } from "../../../services/gameSchedule";
 
 const ValorantGameSchedule = () => {
   const [schedules, setSchedules] = useState([]);
 
   useEffect(() => {
-    const fetchSchedules = async () => {
+    const getSchedules = async () => {
       try {
-        const response = await axios.get(
-          "http://localhost:5000/upcomingGames?limit=4&game=valorant"
-        );
-        setSchedules(
-          Array.isArray(response.data.data) ? response.data.data : []
-        );
+        const data = await fetchHomeGameSchedule();
+        setSchedules(data);
       } catch (error) {
         console.error("Error fetching schedules:", error);
         setSchedules([]);
       }
     };
 
-    fetchSchedules();
+    getSchedules();
   }, []);
+
   return (
     <div className="px-8 pb-10 gap-4 md:px-4 md:gap-y-0 lg:gap-x-2 lg:gap-y-0 lg:px-2 grid grid-cols-1 sm:grid-cols-2 3xl:grid-cols-4 3xl:px-34">
       {schedules.map((schedule, index) => (
         <div
           key={schedule.id || index}
-          className="card bg-zinc-800 w-auto lg:w-auto 3xl:w-auto shadow-xl text-center text-white items-center mb-5"
+          className="card bg-zinc-800 w-auto lg:w-auto 3xl:w-auto shadow-xl text-center text-white items-center mb-5 pt-8"
         >
           <h2 className="card-title mt-2 text-lg px-2">
             {schedule.match_event}
           </h2>
           <div className="card-body items-center text-center min-h-64 flex flex-col justify-center">
             <div className="flex items-center justify-between w-full">
-              {/* Team 1 */}
               <div className="flex flex-col items-center w-1/3">
                 <img
                   src={schedule.logo1}
@@ -43,7 +38,7 @@ const ValorantGameSchedule = () => {
                 />
                 <p className="team-name text-sm mt-2">{schedule.team1}</p>
               </div>
-              {/* Match Info */}
+
               <div className="flex flex-col justify-center items-center w-1/3">
                 <h2 className="game-date card-title text-sm font-thin">
                   {new Date(schedule.unix_timestamp).toLocaleDateString(
@@ -70,7 +65,7 @@ const ValorantGameSchedule = () => {
                   PHT
                 </p>
               </div>
-              {/* Team 2 */}
+
               <div className="flex flex-col items-center w-1/3">
                 <img
                   src={schedule.logo2}
