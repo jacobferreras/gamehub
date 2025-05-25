c787f639ba653f3a4159749b134cf89671fa8a7a
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-05-25 13:03:01 +0800

    Remove unused components: Loader, AccountCard, HeadNewsCard, NewsCard, NewsCardSide, and PubgGameSchedule

--- frontend/src/components/Loader.jsx
+++
@@ -1,8 +0,0 @@
-const Loader = () => (
-  <div className="flex items-center justify-center min-h-screen bg-neutral-950">
-    <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
-    <span className="ml-4 text-white text-lg">Loading...</span>
-  </div>
-);
-
-export default Loader;
