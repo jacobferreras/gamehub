bf71a3b494adeebc87705d1ece6833ff14cc979d
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-05-25 12:37:40 +0800

    Add article links to news cards in LargeScreenNews and SmallScreenNews components

--- frontend/src/components/ui/HomeScreen/LargeScreenNews.jsx
+++ frontend/src/components/ui/HomeScreen/LargeScreenNews.jsx
@@ -27,61 +27,97 @@ const LargeScreenNews = () => {
   return (
     <div className="hidden sm:grid sm:grid-cols-1 sm:gap-2 px-6 lg:grid-cols-3">
       <div className="row-span-2 col-span-2">
-        <HeadNewsCard
-          Title={valorantArticles[0]?.title}
-          Description={valorantArticles[0]?.description}
-          Date={valorantArticles[0]?.date}
-          Author={valorantArticles[0]?.author}
-          ImageUrl={valorantArticles[0]?.image}
-        />
+        <a
+          href={valorantArticles[0]?.article_url}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <HeadNewsCard
+            Title={valorantArticles[0]?.title}
+            Description={valorantArticles[0]?.description}
+            Date={valorantArticles[0]?.date}
+            Author={valorantArticles[0]?.author}
+            ImageUrl={valorantArticles[0]?.image}
+          />
+        </a>
       </div>
 
       <div className="sm:hidden lg:block lg:col-span-1 lg:row-span-2 lg:col-start-3 lg:row-start-1">
-        <NewsCard
-          Title={valorantArticles[1]?.title}
-          Description={valorantArticles[1]?.description}
-          Date={valorantArticles[1]?.date}
-          Author={valorantArticles[1]?.author}
-          ImageUrl={valorantArticles[1]?.image}
-        />
-      </div>
-
-      <div className="col-span-2 grid grid-cols-2 lg:col-span-3 lg:grid-cols-3 gap-2">
-        <div className="pb-2">
-          <NewsCardSide
-            Title={valorantArticles[2]?.title}
-            Description={valorantArticles[2]?.description}
-            Date={valorantArticles[2]?.date}
-            Author={valorantArticles[2]?.author}
-            ImageUrl={valorantArticles[2]?.image}
-          />
-        </div>
-        <div className="pb-2">
-          <NewsCardSide
-            Title={valorantArticles[3]?.title}
-            Description={valorantArticles[3]?.description}
-            Date={valorantArticles[3]?.date}
-            Author={valorantArticles[3]?.author}
-            ImageUrl={valorantArticles[3]?.image}
-          />
-        </div>
-        <div className="pb-2">
-          <NewsCardSide
-            Title={valorantArticles[4]?.title}
-            Description={valorantArticles[4]?.description}
-            Date={valorantArticles[4]?.date}
-            Author={valorantArticles[4]?.author}
-            ImageUrl={valorantArticles[4]?.image}
-          />
-        </div>
-        <div className="pb-2 lg:hidden">
-          <NewsCardSide
+        <a
+          href={valorantArticles[1]?.article_url}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <NewsCard
             Title={valorantArticles[1]?.title}
             Description={valorantArticles[1]?.description}
             Date={valorantArticles[1]?.date}
             Author={valorantArticles[1]?.author}
             ImageUrl={valorantArticles[1]?.image}
           />
+        </a>
+      </div>
+
+      <div className="col-span-2 grid grid-cols-2 lg:col-span-3 lg:grid-cols-3 gap-2">
+        <div className="pb-2">
+          <a
+            href={valorantArticles[2]?.article_url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <NewsCardSide
+              Title={valorantArticles[2]?.title}
+              Description={valorantArticles[2]?.description}
+              Date={valorantArticles[2]?.date}
+              Author={valorantArticles[2]?.author}
+              ImageUrl={valorantArticles[2]?.image}
+            />
+          </a>
+        </div>
+        <div className="pb-2">
+          <a
+            href={valorantArticles[3]?.article_url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <NewsCardSide
+              Title={valorantArticles[3]?.title}
+              Description={valorantArticles[3]?.description}
+              Date={valorantArticles[3]?.date}
+              Author={valorantArticles[3]?.author}
+              ImageUrl={valorantArticles[3]?.image}
+            />
+          </a>
+        </div>
+        <div className="pb-2">
+          <a
+            href={valorantArticles[4]?.article_url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <NewsCardSide
+              Title={valorantArticles[4]?.title}
+              Description={valorantArticles[4]?.description}
+              Date={valorantArticles[4]?.date}
+              Author={valorantArticles[4]?.author}
+              ImageUrl={valorantArticles[4]?.image}
+            />
+          </a>
+        </div>
+        <div className="pb-2 lg:hidden">
+          <a
+            href={valorantArticles[1]?.article_url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <NewsCardSide
+              Title={valorantArticles[1]?.title}
+              Description={valorantArticles[1]?.description}
+              Date={valorantArticles[1]?.date}
+              Author={valorantArticles[1]?.author}
+              ImageUrl={valorantArticles[1]?.image}
+            />
+          </a>
         </div>
       </div>
     </div>
--- frontend/src/components/ui/HomeScreen/SmallScreenNews.jsx
+++ frontend/src/components/ui/HomeScreen/SmallScreenNews.jsx
@@ -27,50 +27,80 @@ const SmallScreenNews = () => {
   return (
     <div className="block sm:hidden">
       <div className="justify-start pb-3 px-2">
-        <HeadNewsCard
-          Title={articles[0]?.title}
-          Description=""
-          Date={articles[0]?.date}
-          Author={articles[0]?.author}
-          ImageUrl={articles[0]?.image}
-        />
+        <a
+          href={articles[0]?.article_url}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <HeadNewsCard
+            Title={articles[0]?.title}
+            Description=""
+            Date={articles[0]?.date}
+            Author={articles[0]?.author}
+            ImageUrl={articles[0]?.image}
+          />
+        </a>
       </div>
       <div className="block px-2 md:px-4">
         <div className="pb-2">
-          <NewsCardSide
-            Title={articles[1]?.title}
-            Description=""
-            Date={articles[1]?.date}
-            Author={articles[1]?.author}
-            ImageUrl={articles[1]?.image}
-          />
+          <a
+            href={articles[1]?.article_url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <NewsCardSide
+              Title={articles[1]?.title}
+              Description=""
+              Date={articles[1]?.date}
+              Author={articles[1]?.author}
+              ImageUrl={articles[1]?.image}
+            />
+          </a>
         </div>
         <div className="pb-2">
-          <NewsCardSide
-            Title={articles[2]?.title}
-            Description=""
-            Date={articles[2]?.date}
-            Author={articles[2]?.author}
-            ImageUrl={articles[2]?.image}
-          />
+          <a
+            href={articles[2]?.article_url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <NewsCardSide
+              Title={articles[2]?.title}
+              Description=""
+              Date={articles[2]?.date}
+              Author={articles[2]?.author}
+              ImageUrl={articles[2]?.image}
+            />
+          </a>
         </div>
         <div className="pb-2">
-          <NewsCardSide
-            Title={articles[3]?.title}
-            Description=""
-            Date={articles[3]?.date}
-            Author={articles[3]?.author}
-            ImageUrl={articles[3]?.image}
-          />
+          <a
+            href={articles[3]?.article_url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <NewsCardSide
+              Title={articles[3]?.title}
+              Description=""
+              Date={articles[3]?.date}
+              Author={articles[3]?.author}
+              ImageUrl={articles[3]?.image}
+            />
+          </a>
         </div>
         <div className="pb-2">
-          <NewsCardSide
-            Title={articles[4]?.title}
-            Description=""
-            Date={articles[4]?.date}
-            Author={articles[4]?.author}
-            ImageUrl={articles[4]?.image}
-          />
+          <a
+            href={articles[4]?.article_url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <NewsCardSide
+              Title={articles[4]?.title}
+              Description=""
+              Date={articles[4]?.date}
+              Author={articles[4]?.author}
+              ImageUrl={articles[4]?.image}
+            />
+          </a>
         </div>
       </div>
     </div>
