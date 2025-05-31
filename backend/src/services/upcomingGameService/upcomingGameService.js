04f27721ca76d48c546f09545ab942a7a67b8680
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-05-31 17:22:38 +0800

    Update cron job logic to move expired games based on a 8-hour interval

--- backend/src/services/upcomingGameService/upcomingGameService.js
+++ backend/src/services/upcomingGameService/upcomingGameService.js
@@ -58,14 +58,15 @@ const getAll = async ({
 };
 
 const moveToGameResults = async () => {
+  console.log("Cron job running: moving expired games...");
   await db.promise().query(
     `INSERT INTO game_result(id, game, team1, team2, logo1, logo2, match_series, match_event, region)
-       SELECT id, game, team1, team2, logo1, logo2, match_series, match_event, region FROM upcoming_games WHERE unix_timestamp < NOW()`
+       SELECT id, game, team1, team2, logo1, logo2, match_series, match_event, region FROM upcoming_games WHERE unix_timestamp < DATE_ADD(NOW(), INTERVAL 8 HOUR)`
   );
-
   await db.promise().query(`
-    DELETE FROM upcoming_games WHERE unix_timestamp < NOW()
+    DELETE FROM upcoming_games WHERE unix_timestamp < DATE_ADD(NOW(), INTERVAL 8 HOUR)
   `);
+  console.log("Cron job finished.");
 };
 cron.schedule("* * * * *", moveToGameResults);
 
