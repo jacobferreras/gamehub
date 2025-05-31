c0107f4ee259f08f1549e499bd0b5f63c1669fb7
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-06-01 01:50:28 +0800

    Refactor button styles in NewsNavigation component for consistency

--- frontend/src/components/common/NewsNavigation.jsx
+++ frontend/src/components/common/NewsNavigation.jsx
@@ -6,14 +6,14 @@ const NewsNavigation = () => {
     <div className="flex justify-start gap-2 pl-10 pt-24">
       <Link to="/app/news/article">
         <button
-          className={`btn bg-white/10 opacity-30 text-white border-2 btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl transition-transform duration-150 active:scale-75 ${location.pathname === "/app/news/article" ? "bg-blue-500 text-white scale-105 shadow-lg opacity-200" : ""}`}
+          className={`btn bg-white/10 opacity-30 text-white border-2 btn-lg  xl:btn-xl transition-transform duration-150 active:scale-75 ${location.pathname === "/app/news/article" ? "bg-blue-500 text-white scale-105 shadow-lg opacity-200" : ""}`}
         >
           Article
         </button>
       </Link>
       <Link to="/app/news/update">
         <button
-          className={`btn border-2 bg-white/10 opacity-30 text-white  btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl transition-transform duration-150 active:scale-75 ${location.pathname === "/app/news/update" ? "bg-blue-500 text-white scale-105 shadow-lg opacity-200" : ""}`}
+          className={`btn border-2 bg-white/10 opacity-30 text-white  btn-lg xl:btn-xl transition-transform duration-150 active:scale-75 ${location.pathname === "/app/news/update" ? "bg-blue-500 text-white scale-105 shadow-lg opacity-200" : ""}`}
         >
           Update
         </button>
