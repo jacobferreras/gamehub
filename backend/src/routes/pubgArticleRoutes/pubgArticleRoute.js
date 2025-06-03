6d2c3f82d35f4390f9e7b02900d247a4f1c8a60c
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-06-03 22:37:54 +0800

    Remove PUBG article and schedule controllers and routes for deprecation

--- backend/src/routes/pubgArticleRoutes/pubgArticleRoute.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import pubgArticleController from "../../controllers/pubgArticleController/pubgArticleController.js";
-
-const router = Router();
-
-router.get("/", pubgArticleController.getPubgArticles);
-
-export default router;
